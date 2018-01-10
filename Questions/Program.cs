8161005b611c70f9c8456c02ebef00f23f244103
pathak.shivam <shivam.pathak@gmail.com>
2018-01-10 15:47:35 +0530

    updated

--- Questions/Program.cs
+++ Questions/Program.cs
@@ -35,6 +35,8 @@ namespace Questions
             {
                 char[] x = input.ToCharArray();
 
+                     
+
                 if (!repated[x[i]])
                 {
                     if (!output.Contains(x[i]))
